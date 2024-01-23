480c6801f6ca17f529e30a49d7718a03d4e97dc3
Daniil Sorokin <157469356+Daniil056@users.noreply.github.com>
2024-01-23 18:32:43 +0700

    Update Sorokin_1ISP-23.js

--- Sorokin_1ISP-23.js
+++ Sorokin_1ISP-23.js
@@ -1,1 +1,7 @@
-alert 
+alert('Выполним сложение!')
+let a = parseFloat(prompt('Введите число a:'))
+let b = parseFloat(prompt('Введите число b:'))
+let plus = +a + +b
+alert('Сложение выполнено!')
+alert(plus)
+console.log(plus)
