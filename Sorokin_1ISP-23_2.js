f995b2a961f2695531832843b0b2c2316bc049be
Daniil Sorokin <157469356+Daniil056@users.noreply.github.com>
2024-01-23 20:00:00 +0700

    Скрипт на произведение двух чисел

---
+++ Sorokin_1ISP-23_2.js
@@ -0,0 +1,7 @@
+alert('Выполним умножение!')
+let a = parseFloat(prompt('Введите число a:'))
+let b = parseFloat(prompt('Введите число b:'))
+let plus = +a * +b
+alert('Произведение выполнено!')
+alert(plus)
+console.log(plus)
