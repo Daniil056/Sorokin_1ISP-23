cb8a2eb5d62af794f964aadf4eb4862a2bdf2503
Daniil Sorokin <157469356+Daniil056@users.noreply.github.com>
2024-01-23 20:09:42 +0700

    Скрипт на деление двух чисел

---
+++ Sorokin_1ISP-23_3.js
@@ -0,0 +1,7 @@
+alert('Выполним деление!')
+let a = parseFloat(prompt('Введите число a:'))
+let b = parseFloat(prompt('Введите число b:'))
+let plus = +a / +b
+alert('Деление выполнено!')
+alert(plus)
+console.log(plus)
